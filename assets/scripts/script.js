50d9031eae234c654fb07b7049ca92a606622cc3
EASYMAK777 <jplocallawncare@gmail.com>
2021-01-19 10:20:30 -0500

    created a function to open modal when appointment button is clicked

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,11 @@
 //Modal//
 
-var myModal = document.getElementById('submitButton')
+var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
 
+myModal.addEventListener('shown.bs.modal' , function() {
+    myInput.focus()
+})
 
 
 
